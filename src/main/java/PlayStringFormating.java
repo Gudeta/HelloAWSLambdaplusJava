7660f28510219bbf8269f81ad2633a1e4fdb4227
Gudeta <gudetabesso@gmail.com>
2023-02-28 17:14:42 -0600

    String, Java 8

---
+++ src/main/java/PlayStringFormating.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class PlayStringFormating {
+    public static void main(String[] args) {
+        String ph1="90-123-3344";
+        String ph2="(90)-123-3344";
+       /* System.out.println(ph1);
+        ph1 = ph1.replace("-", "");*/
+        System.out.println(ph1);
+        StringBuffer sb = new StringBuffer();
+
+        /**The code filters out the numbers divisible by 4 (20 and 52), doubles them (40 and 104), and then adds them together (144). The reduce() method starts with an initial value of 0 and applies the Integer::sum function to each element of the stream to compute the final sum of 144.*/
+        List<Integer> numbers = Arrays.asList(11, 20, 33, 45, 52);
+        int sum = numbers.stream()
+                .filter(n -> n % 4 == 0)
+                .map(n -> n * 2)
+                .reduce(0, Integer::sum);
+        System.out.println(sum);
+    }
+}
